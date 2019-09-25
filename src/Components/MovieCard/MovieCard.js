7e90eb026f27fb920357e0bc032a0c3f0eb20e75
”bexi”  <trreitmaier@msn.com>
2019-09-25 10:42:54 +0200

    movie card css is now ok

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import {AppContext} from "../../App";
 
+import './movie-card.css';
+
 const MovieCard = (props) => {
     const App = useContext(AppContext);
 
@@ -10,13 +12,15 @@ const MovieCard = (props) => {
     //const [movieImage, setMovieImage] = useState(""); // implement later
 
     return (
-        <div className='row'>
-            <div className='col-5 float-left'>
-                <div>Title: {title}</div>
-                <div>Rating: {rating}</div>
-                <div>Watch date: TODO </div>
+        <div className="container">
+            <div className='row movie-card'>
+                <div className='col-5 float-left'>
+                    <div>Title: {title}</div>
+                    <div>Rating: {rating}</div>
+                    <div>Watch date: TODO </div>
+                </div>
+                <div className='col float-right movie-image'>TODO IMAGE</div>
             </div>
-            <div className='col float-right movie-image'>TODO IMAGE</div>
         </div>
     );
 };
